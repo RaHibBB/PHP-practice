70c2ce1acf2598f773d6ee62e331b4dfe9baa359
RaHibBB <mdrahibchowdhury@gmail.com>
2023-11-16 21:26:54 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// $name = "Rahib Chowdhury" .PHP_EOL;
+// echo $name;
+// $a = 5;
+// $b = 5;
+// $sum = $a+$b;
+// echo $sum
+
+
+// $name = 'jhon';
+// $age = '500';
+// echo "my age is $age and my name is $name";
+// echo ' My age is'. ' '  .$age .' and my name is ' . ' ' . $name;
+// echo "Hello, My \"age\" is $age" ."\n";
+// echo "<a href= \"https://google.com\"> Google </a>";
+// echo '<a href= "https://google.com"> Google </a>';
+
+const NID = 3753772239;
+var_dump(NID);
+// $nid = NID;
+
+echo "My NID is";
+
+
