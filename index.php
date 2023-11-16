1190490815cd3624c1604394c89c82239bdccb0e
RaHibBB <mdrahibchowdhury@gmail.com>
2023-11-16 21:42:04 +0600

    second

--- index.php
+++ index.php
@@ -15,10 +15,33 @@
 // echo "<a href= \"https://google.com\"> Google </a>";
 // echo '<a href= "https://google.com"> Google </a>';
 
-const NID = 3753772239;
-var_dump(NID);
+// const NID = 3753772239;
+// var_dump(NID);
 // $nid = NID;
 
-echo "My NID is";
+// echo "My NID is";
+
+// $foo = 25;
+// $bar = &$foo ;
+// echo $bar;
+
+// $name = "iftakhar";
+// echo "My name is {$name}";
+
+//null ,bull ,inti ,float ,object,array,resource;
+// $name ='iftakhar';
+// $null = ['a','b','c'];
+// echo get_debug_type($null);
+// var_dump($null);
+
+$name = 33;
+if (is_string($name)) {
+    echo $name;
+    
+}
+else{
+    echo "It's not a string";
+}
+
 
 
